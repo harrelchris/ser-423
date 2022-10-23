782b13e6af3898cefadf5f44329dedc60701f20e
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-10-23 03:17:30 -0500

    remove old commented code

--- exercise_2/App.js
+++ exercise_2/App.js
@@ -39,21 +39,3 @@ const styles = StyleSheet.create({
     margin: 10,
   },
 });
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
