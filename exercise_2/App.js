3d139661a7f747939c4724cc0ba99061da46124d
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-10-23 22:21:44 -0500

    use ScrollView

--- exercise_2/App.js
+++ exercise_2/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import {Alert, ScrollView, StyleSheet} from 'react-native';
 import Button from './Button';
 
 export default class App extends React.Component {
@@ -10,16 +10,16 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button style={styles.btn}>My first button</Button>
-        <Button success style={styles.btn}>Success button</Button>
-        <Button info style={styles.btn}>Info button</Button>
-        <Button danger rounded style={styles.btn} onPress={this.handleButtonPress}>Rounded button</Button>
-        <Button primary style={styles.btn}>Primary button</Button>
-        <Button secondary rounded style={styles.btn}>Secondary button</Button>
-        <Button warning style={styles.btn}>Warning button</Button>
-        <Button dark rounded style={styles.btn}>Dark button</Button>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Button contentContainerStyle={styles.btn}>My first button</Button>
+        <Button success contentContainerStyle={styles.btn}>Success button</Button>
+        <Button info contentContainerStyle={styles.btn}>Info button</Button>
+        <Button danger rounded contentContainerStyle={styles.btn} onPress={this.handleButtonPress}>Rounded button</Button>
+        <Button primary contentContainerStyle={styles.btn}>Primary button</Button>
+        <Button secondary rounded contentContainerStyle={styles.btn}>Secondary button</Button>
+        <Button warning contentContainerStyle={styles.btn}>Warning button</Button>
+        <Button dark rounded contentContainerStyle={styles.btn}>Dark button</Button>
+      </ScrollView>
     );
   }
 }
