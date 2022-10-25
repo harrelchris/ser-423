56c7d1b9d5e46f175db3221727cd0cf687682177
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-10-25 01:18:37 -0500

    Add UI features

--- assignment_2/App.js
+++ assignment_2/App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView} from "react-native";
+import {WebView} from "react-native-webview";
+import Button from "./Button";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const links = [
+    {'title': 'Asia', 'url': 'https://en.wikipedia.org/wiki/Asia'},
+    {'title': 'Africa', 'url': 'https://en.wikipedia.org/wiki/Africa'},
+    {'title': 'Europe', 'url': 'https://en.wikipedia.org/wiki/Europe'},
+    {'title': 'Australia', 'url': 'https://en.wikipedia.org/wiki/Australia'},
+    {'title': 'Antarctica', 'url': 'https://en.wikipedia.org/wiki/Antarctica'},
+    {'title': 'North America', 'url': 'https://en.wikipedia.org/wiki/North_America'},
+    {'title': 'South America', 'url': 'https://en.wikipedia.org/wiki/South_America'},
+  ]
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [url, setUrl] = useState('https://en.wikipedia.org')
+
+  return (
+    <ScrollView>
+      <WebView source={{uri: url}} style={{height: 319}}/>
+      <Button onPress={() => setUrl(links[0].url)} dark>Asia</Button>
+      <Button onPress={() => setUrl(links[1].url)} light>Africa</Button>
+      <Button onPress={() => setUrl(links[2].url)} danger>Europe</Button>
+      <Button onPress={() => setUrl(links[3].url)} primary>Australia</Button>
+      <Button onPress={() => setUrl(links[4].url)} success>Antarctica</Button>
+      <Button onPress={() => setUrl(links[5].url)} warning>North America</Button>
+      <Button onPress={() => setUrl(links[6].url)} secondary>South America</Button>
+    </ScrollView>
+  )
+}
