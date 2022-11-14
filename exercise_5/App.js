58eed65a95ccdf8cf31f8131721a08aeb261e0da
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-11-14 02:22:43 -0600

    unworking WIP

--- exercise_5/App.js
+++ exercise_5/App.js
@@ -1,13 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React, { Component } from 'react';
+// import {
+//   Location,
+//   Permissions,
+//   MapView,
+//   Marker
+// } from 'expo';
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+// } from 'react-native';
+//
+// export default class App extends Component {
+//   state = {
+//     location: null
+//   }
+//
+//   async componentDidMount() {
+//     try {
+//       const permission = await Permissions.askAsync(Permissions.LOCATION);
+//       if (permission && permission.status === 'granted') {
+//         this.getLocation();
+//       }
+//     } catch (e) {
+//       console.log(e)
+//     }
+//
+//   }
+//
+//   async getLocation() {
+//     let location = await Location.getCurrentPositionAsync({});
+//     this.setState({
+//       location
+//     });
+//   }
+//
+//   renderMap() {
+//     return this.state.location ?
+//       <MapView
+//         style={styles.map}
+//         initialRegion={{
+//           latitude: this.state.location.coords.latitude,
+//           longitude: this.state.location.coords.longitude,
+//           latitudeDelta: 0.09,
+//           longitudeDelta: 0.04,
+//         }}
+//       >
+//         {/*<MapView.Marker*/}
+//         {/*  coordinate={this.state.location.coords}*/}
+//         {/*  title={"User Location"}*/}
+//         {/*  description={"You are here!"}*/}
+//         {/*  image={require('./assets/you-are-here.png')}*/}
+//         {/*/>*/}
+//     </MapView> : null
+//   }
+//
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         {this.renderMap()}
+//       </View>
+//     );
+//   }
+//
+// }
+//
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+//   map: {
+//     flex: 1
+//   }
+// });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+import React, { Component } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+
+export default class App extends Component {
+  state = {
+    location: null,
+  }
+
+  async componentDidMount() {
+    try {
+      const permission = await Permissions.askAsync(Permissions.LOCATION);
+      const location = await Location.getCurrentPositionAsync()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  render() {
+    return this.state.location ?
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: this.state.location.coords.latitude,
+          longitude: this.state.location.coords.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+      ></MapView> : null
+
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +121,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+})
